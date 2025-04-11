fa4c27e71259bf95bdcaa18ad7f4cc61165a5979
Aviya Edri <160749923+AviyaEdri@users.noreply.github.com>
2025-04-11 17:04:17 +0300

    Add files via upload

--- MyMazeGenerator.java
+++ MyMazeGenerator.java
@@ -48,64 +48,102 @@ public class MyMazeGenerator extends AMazeGenerator {
                 addWalls(maze, cell1[0], cell1[1], walls); // Add walls around the new cell
             }
         }
-        Maze Mymaze = new Maze(); // Create a new maze object
+
+        // Choose random internal cells for start and goal
+        Position innerStart = getRandomOpenCell(maze, random);
+        Position innerGoal = getRandomOpenCell(maze, random);
+        while (innerGoal.equals(innerStart)) {
+            innerGoal = getRandomOpenCell(maze, random);
+        }
+
+        // Open entrance/exit on edge near inner cells
+        Position start = openEdgeNear(innerStart, maze);
+        Position goal = openEdgeNear(innerGoal, maze);
+
+        Maze Mymaze = new Maze();
         Mymaze.setRows(row); // Set the number of rows
         Mymaze.setColumns(col); // Set the number of columns
         Mymaze.setMaze(maze); // Set the maze array
-        Mymaze.setStartPosition(new Position(startRow, startCol)); // Set the starting position
-        Mymaze.setGoalPosition(new Position(row - 1, col - 1)); // Set the goal position
+        Mymaze.setStartPosition(start);
+        Mymaze.setGoalPosition(goal);
+
         return Mymaze; // Return the generated maze
     }
 
-
-    /**
-     * This function adds walls around the current cell to the list of walls.
-     * @param maze
-     * @param row
-     * @param col
-     * @param walls
-     */
     private void addWalls(int[][] maze, int row, int col, List<int[]> walls) {
         int[][] directions = {
-            {-2, 0}, // Up
-            {2, 0},  // Down
-            {0, -2}, // Left
-            {0, 2}   // Right
+                {-2, 0}, {2, 0}, {0, -2}, {0, 2}
         };
-
-        for(int[] direction : directions){
-            int newRow = row + direction[0];
-            int newCol = col + direction[1];
-
-            if(newRow > 0 && newRow < maze.length &&  newCol > 0 && newCol <maze[0].length){
-                int wallRow = row + direction[0] / 2; // Wall row
-                int wallCol = col + direction[1] / 2; // Wall column
-                if(maze[newRow][newCol] == 1){
-                    walls.add(new int[]{wallRow, wallCol}); // Add wall to the list
+        for (int[] dir : directions) {
+            int newRow = row + dir[0];
+            int newCol = col + dir[1];
+            if (newRow > 0 && newRow < maze.length && newCol > 0 && newCol < maze[0].length) {
+                int wallRow = row + dir[0] / 2;
+                int wallCol = col + dir[1] / 2;
+                if (maze[newRow][newCol] == 1) {
+                    walls.add(new int[]{wallRow, wallCol});
                 }
             }
         }
     }
-
     /**
-     * This function gets the neighbors of the current cell in the maze.
-     * @param row
-     * @param col
-     * @param maze
-     * @return
+     * Get the neighbors of a wall cell.
+     *
+     * @param row the row index of the wall
+     * @param col the column index of the wall
+     * @param maze the maze array
+     * @return an array of neighbor positions
      */
-    private int[][] getNeighbors(int row, int col, int[][] maze){
-        if(row % 2 == 1 && col % 2 == 0){
-            return new int[][]{
-                    {row, col - 1}, // Left
-                    {row, col + 1}, // Right
-            };
-        } else if(row % 2 == 0 && col % 2 == 1){
-            return new int[][]{
-                    {row - 1, col}, // Up
-                    {row + 1, col}, // Down
-            };
+    private int[][] getNeighbors(int row, int col, int[][] maze) {
+        if (row % 2 == 1 && col % 2 == 0) {
+            return new int[][]{{row, col - 1}, {row, col + 1}};
+        } else if (row % 2 == 0 && col % 2 == 1) {
+            return new int[][]{{row - 1, col}, {row + 1, col}};
         }
-        return null; // No neighbors
+        return null;
+    }
+    /**
+     * Get a random open cell in the maze.
+     *
+     * @param maze the maze array
+     * @param rand the random number generator
+     * @return a Position object representing the random open cell
+     */
+    private Position getRandomOpenCell(int[][] maze, Random rand) {
+        int row, col;
+        do {
+            row = 2 * rand.nextInt((maze.length - 1) / 2) + 1;
+            col = 2 * rand.nextInt((maze[0].length - 1) / 2) + 1;
+        } while (maze[row][col] != 0);
+        return new Position(row, col);
+    }
+    /**
+     * Open an edge near the given cell in the maze.
+     *
+     * @param cell the cell position
+     * @param maze the maze array
+     * @return a Position object representing the opened edge
+     */
+    private Position openEdgeNear(Position cell, int[][] maze) {
+        int row = cell.getRowIndex();
+        int col = cell.getColumnIndex();
+
+        if (row == 1) {
+            maze[0][col] = 0;
+            return new Position(0, col);
+        } else if (row == maze.length - 2) {
+            maze[maze.length - 1][col] = 0;
+            return new Position(maze.length - 1, col);
+        } else if (col == 1) {
+            maze[row][0] = 0;
+            return new Position(row, 0);
+        } else if (col == maze[0].length - 2) {
+            maze[row][maze[0].length - 1] = 0;
+            return new Position(row, maze[0].length - 1);
+        }
+
+        // fallback
+        maze[row][0] = 0;
+        return new Position(row, 0);
     }
 }
