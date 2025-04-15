e32b4f4575fee3a53df95b6cad1c4433af1916ac
Aviya Edri <aviya254@gmail.com>
2025-04-15 13:56:07 +0300

    delete doubl

--- src/JUnit/algorithms/mazeGenerators/AMazeGeneratorTest.java
+++ src/JUnit/algorithms/mazeGenerators/AMazeGeneratorTest.java
@@ -1,4 +1,14 @@
+package algorithms.mazeGenerators;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class AMazeGeneratorTest {
-  
+
+    @org.junit.jupiter.api.Test
+    void generate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void measureAlgorithmTimeMillis() {
+    }
 }
